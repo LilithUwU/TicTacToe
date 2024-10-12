bc23e4e7d9328ff477de80d2a89b4721927fd68a
lilit <lilith.takiryan@gmail.com>
2024-10-13 01:33:46 +0400

    adopting logic for showing winning cells

--- app/src/main/java/com/example/toctactoe/GameActivity.kt
+++ app/src/main/java/com/example/toctactoe/GameActivity.kt
@@ -2,12 +2,16 @@ package com.example.toctactoe
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.DragEvent
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import com.example.toctactoe.databinding.ActivitySecondBinding
+import com.google.android.material.button.MaterialButton
 
 class GameActivity : AppCompatActivity() {
     private val binding by lazy {
@@ -22,7 +26,8 @@ class GameActivity : AppCompatActivity() {
     private lateinit var player1: String
     private lateinit var player2: String
     private lateinit var resultIntent: Intent
-    private lateinit var cellViews: List<TextView>
+    private lateinit var cellViews: Array<Array<MaterialButton>>
+    private lateinit var greenBackground: Drawable
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,16 +37,17 @@ class GameActivity : AppCompatActivity() {
         player1 = intent.getStringExtra(Constants.INTENT_PLAYER1_NAME).toString()
         player2 = intent.getStringExtra(Constants.INTENT_PLAYER2_NAME).toString()
         binding.hint.text = getString(R.string.it_s_your_turn, player1)
+        greenBackground = AppCompatResources.getDrawable(this, R.drawable.shape_cell_win)!!
 
         cellSignsArray = arrayOf(
             arrayOf("", "", ""),
             arrayOf("", "", ""),
             arrayOf("", "", "")
         )//later will be set the and used in checking the game result
-        cellViews = listOf(
-            binding.p00, binding.p01, binding.p02,
-            binding.p10, binding.p11, binding.p12,
-            binding.p20, binding.p21, binding.p22
+        cellViews = arrayOf(
+            arrayOf(binding.p00, binding.p01, binding.p02),
+            arrayOf(binding.p10, binding.p11, binding.p12),
+            arrayOf(binding.p20, binding.p21, binding.p22)
         )
         Toast.makeText(this, "$player1 & $player2", Toast.LENGTH_SHORT).show()
         val buttonClickListener = View.OnClickListener { view ->
@@ -148,29 +154,60 @@ class GameActivity : AppCompatActivity() {
         for (i in arr.indices) {
             //check for rows
             if (arr[i][0] == arr[i][1] && arr[i][1] == arr[i][2]) {
-                checkHelperLogic(i, 0, arr)
+                if (checkHelperLogic(i, 0, arr).second) {
+                    cellViews[i][0].background = getDrawable(R.drawable.shape_cell_win)
+                    cellViews[i][1].background = getDrawable(R.drawable.shape_cell_win)
+                    cellViews[i][2].background = getDrawable(R.drawable.shape_cell_win)
+
+                }
+                for (row in cellViews) {
+                    for (cell in row) {
+                        cell.invalidate() // Enable or disable each cell based on the state
+                    }
+                }
             }
             //check for columns
             if (arr[0][i] == arr[1][i] && arr[1][i] == arr[2][i]) {
-                checkHelperLogic(0, i, arr)
+                if (checkHelperLogic(0, i, arr).second) {
+                    cellViews[0][i].background = greenBackground
+                    cellViews[1][i].background = greenBackground
+                    cellViews[2][i].background = greenBackground
+                }
+
             }
         }
         //check for diagonals
         if (arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2]) {
-            checkHelperLogic(0, 0, arr)
+            if (checkHelperLogic(0, 0, arr).second) {
+                cellViews[0][0].background = greenBackground
+                cellViews[1][1].background = greenBackground
+                cellViews[2][2].background = greenBackground
+            }
         }
         if (arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0]) {
-            checkHelperLogic(0, 2, arr)
+            if (checkHelperLogic(0, 2, arr).second) {
+                cellViews[0][2].background = greenBackground
+                cellViews[1][1].background = greenBackground
+                cellViews[2][0].background = greenBackground
+            }
         } else return Pair("Draw", false)
         return Pair("Error", false)
     }
 
     private fun enableBtns(state: Boolean) {
-        cellViews.forEach { it.isEnabled = state }
+        for (row in cellViews) {
+            for (cell in row) {
+                cell.isEnabled = state // Enable or disable each cell based on the state
+            }
+        }
     }
 
     private fun setListenersForCells(buttonClickListener: View.OnClickListener) {
-        cellViews.forEach { it.setOnClickListener(buttonClickListener) }
+        for (row in cellViews) {
+            for (cell in row) {
+                cell.setOnClickListener(buttonClickListener) // Set the click listener for each cell
+            }
+        }
     }
 
 
