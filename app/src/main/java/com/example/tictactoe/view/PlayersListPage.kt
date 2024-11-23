053f8755b9d2b2fa1f1c5445ed425567028b00c1
lilit <lilith.takiryan@gmail.com>
2024-11-23 15:55:29 +0400

    add: back button in game history page

--- app/src/main/java/com/example/tictactoe/view/PlayersListPage.kt
+++ app/src/main/java/com/example/tictactoe/view/PlayersListPage.kt
@@ -3,29 +3,35 @@ package com.example.tictactoe.view
 
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayCircleFilled
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -34,12 +40,12 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.tictactoe.Constants
 import com.example.tictactoe.Constants.COMING_FROM
 import com.example.tictactoe.Constants.INTENT_EXTRA_HISTORY
 import com.example.tictactoe.Constants.PLAYER_ID
@@ -48,6 +54,8 @@ import com.example.tictactoe.model.Players
 import com.example.tictactoe.ui.theme.darkPrimaryColor
 import com.example.tictactoe.ui.theme.primaryColor
 import com.example.tictactoe.viewmodel.PlayersListViewModel
+import kotlinx.coroutines.android.awaitFrame
+import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -56,46 +64,85 @@ fun PlayersListPage(viewModel: PlayersListViewModel) {
     val playersList by viewModel.playersList.observeAsState()
     val context = LocalContext.current
 
-    Column(
+    val onBackPressedDispatcher = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
+    var backPressHandled by remember { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
+    BackHandler(enabled = !backPressHandled) {
+        backPressHandled = true
+        coroutineScope.launch {
+            awaitFrame()
+            onBackPressedDispatcher?.onBackPressed()
+            backPressHandled = false
+        }
+    }
+
+    Box(
         modifier = Modifier
-            .wrapContentSize()
+            .fillMaxSize()
             .padding(8.dp)
     ) {
-        Column(
+        Text(
             modifier = Modifier
-                .wrapContentSize()
-                .padding(8.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .align(Alignment.TopCenter)
+                .padding(8.dp)
+                .wrapContentWidth(),
+            text = stringResource(R.string.game_history),
+            fontSize = 30.sp,
+            fontFamily = FontFamily(Font(R.font.lemon)),
+        )
+        Box(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 50.dp)
+                .fillMaxSize()
         ) {
-            Text(
-                text = "Game History", modifier = Modifier.padding(8.dp), fontSize = 30.sp,
-                fontFamily = FontFamily(Font(R.font.lemon)),
-            )
-            playersList?.let {
-                LazyColumn(
-                    content = {
-                        itemsIndexed(it) { index: Int, item: Players ->
-                            PlayersItem(
-                                item = item,
-                                onDelete = { viewModel.deletePlayers(item.id) },
-                                onContinue = { openGame(item.id, context) },
-                            )
-                        }
-                    }
-                )
-            } ?: Text(
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center,
-                text = "No items yet",
-                fontSize = 16.sp
-            )
+            ListView(playersList, viewModel, context)
+        }
+
+        Button(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            onClick = { onBackPressedDispatcher?.onBackPressed() },
+            shape = RoundedCornerShape(0.dp),
+        ) {
+            Text(text = stringResource(R.string.back), fontSize = 25.sp)
         }
     }
 }
 
+
+@Composable
+private fun ListView(
+    playersList: List<Players>?,
+    viewModel: PlayersListViewModel,
+    context: Context
+) {
+    playersList?.let {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(0.dp, 0.dp, 0.dp, 50.dp),
+            content = {
+                itemsIndexed(it) { index: Int, item: Players ->
+                    PlayersItem(
+                        item = item,
+                        onDelete = { viewModel.deletePlayers(item.id) },
+                        onContinue = { openGame(item.id, context) },
+                    )
+                }
+            }
+        )
+    } ?: Text(
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center,
+        text = stringResource(R.string.no_items_yet),
+        fontSize = 16.sp
+    )
+}
+
 fun openGame(id: Int, context: Context) {
-    Log.d("TAGTAG", "playersPage | openGame: id: $id")
     val intent = Intent(context, GameActivity::class.java).apply {
         putExtra(PLAYER_ID, id)
         putExtra(COMING_FROM, INTENT_EXTRA_HISTORY)
@@ -120,7 +167,6 @@ fun PlayersItem(item: Players, onDelete: () -> Unit, onContinue: () -> Unit) {
             )
             .padding(16.dp),
         verticalAlignment = Alignment.CenterVertically
-
     ) {
         Column(
             modifier = Modifier.weight(1f),
@@ -145,7 +191,6 @@ fun PlayersItem(item: Players, onDelete: () -> Unit, onContinue: () -> Unit) {
                 )
             }
         }
-
     }
 }
 
@@ -157,7 +202,7 @@ private fun PlayersData(item: Players) {
             .padding(4.dp),
     ) {
         Text(
-            text = "Last played:", fontSize = 12.sp, color = Color.White,
+            text = stringResource(R.string.last_played), fontSize = 12.sp, color = Color.White,
             modifier = Modifier.padding(0.dp, 0.dp, 8.dp, 0.dp)
         )
         Text(
@@ -181,20 +226,14 @@ private fun PlayersData(item: Players) {
             .fillMaxWidth()
             .padding(4.dp),
     ) {
-        Text(text = "Score: ", fontSize = 12.sp, color = Color.White)
+        Text(text = stringResource(R.string.score), fontSize = 12.sp, color = Color.White)
         Text(text = item.player1Score, fontSize = 12.sp, color = Color.Yellow)
         Text(text = " - ", fontSize = 12.sp, color = Color.Yellow)
         Text(text = item.player2Score, fontSize = 12.sp, color = Color.Yellow)
         Text(
-            text = "Games played: ", fontSize = 12.sp, color = Color.White,
+            text = stringResource(R.string.games_played), fontSize = 12.sp, color = Color.White,
             modifier = Modifier.padding(8.dp, 0.dp, 0.dp, 0.dp)
         )
         Text(text = item.gamesPlayed, fontSize = 12.sp, color = Color.Yellow)
     }
 }
-
-
-
-
-
-
